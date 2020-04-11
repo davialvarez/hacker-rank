9803280d3b647471584ef7c79eadfe7b5570bb94
David Alvarez <david.alvarezz@gmail.com>
2020-04-11 18:17:21 -0400

    Sock Merchant challenge

--- sock_merchant.py
+++ sock_merchant.py
@@ -8,18 +8,23 @@ import sys
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    print(n, ar)
+    count = 0
+    # Unique items (ui)
+    ui = set(ar) 
+    for v in ui:
+        # Counts the number of occurrences in "ar"
+        # and divide by 2 to get the number of pairs
+        # of v
+        count += ar.count(v) // 2
+    # Returns the number of pairs found in "ar"
+    return count
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sockMerchant(n, ar)
+    print(result)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
 
