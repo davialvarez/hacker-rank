9e846299e0d370d1b89a588add5f2b29e1b98f24
David Alvarez <david.alvarezz@gmail.com>
2020-04-13 21:44:07 -0400

    feat: Jumping on the clouds challenge

---
+++ jumping_on_the_clouds.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+
+if __name__ == '__main__':
+
+    n = int(input())
+    c = list(map(int, input().rstrip().split()))
+    result = jumpingOnClouds(c)
+    print(result)
