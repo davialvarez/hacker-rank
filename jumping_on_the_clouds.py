f75bce8e5f35e5b0015acf22fa7a3e2a0f1bf571
David Alvarez <david.alvarezz@gmail.com>
2020-04-14 21:02:54 -0400

    fix: Add return value in function jumpingOnClouds

--- jumping_on_the_clouds.py
+++ jumping_on_the_clouds.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/bin/python
 
 import math
 import os
@@ -8,9 +8,10 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    print(c)
+    return 1
 
 if __name__ == '__main__':
-
     n = int(input())
     c = list(map(int, input().rstrip().split()))
     result = jumpingOnClouds(c)
