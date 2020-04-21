b90ab32dde5ae27095f9a986fe2ad8d8b595794a
David Alvarez <david.alvarezz@gmail.com>
2020-04-21 19:28:05 -0400

    feat: Resolving cases Jumping on the Clouds challenge

--- jumping_on_the_clouds.py
+++ jumping_on_the_clouds.py
@@ -8,14 +8,26 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
-    i = 0
-    count = 0
-
     print(c)
+    count = 0
+    i = 0
+    while i <= len(c):
+        if (c[i] == 0 and c[i+1] == 0):
+            i += 2
+            count += 1 
+            print("Primer caso => Value is: ", c[i], "index is: ", i)
+        elif c[i] == 0:
+            i += 1
+            count += 1
+            print("Segundo caso => Value is: ", c[i], "index is: ", i)
+        elif (c[i] == 0 and c[i+1] == 1):
+            i += 2
+            count += 1
+            print("Tercer caso => Value is: ", c[i], "index is: ", i)
+    print("End for")
     return 1
 
 if __name__ == '__main__':
-    if x == N-1: return 0
     n = int(input())
     c = list(map(int, input().rstrip().split()))
     result = jumpingOnClouds(c)
