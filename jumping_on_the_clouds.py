42c2168fa466d75a6e09aad61c9976c50b313fa5
David Alvarez <david.alvarezz@gmail.com>
2020-04-24 22:11:59 -0400

    fix: Change bucle in Jumping on the clouds challenge

--- jumping_on_the_clouds.py
+++ jumping_on_the_clouds.py
@@ -9,14 +9,14 @@ import sys
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
     count = 0
-    lenght = len(c)
-    for i in range(lenght-1):
-        if c[i+1] == 0:
-            count += 1 
-            print("Find a 0 => Value is: ", c[i], "index is: ", i)
+    i = 0
+    while i != len(c)-1:
+        if i != len(c)-2 and c[i+2] == 0:
+            i += 2
         else:
-            print("Find a 1")
-    return print("Count is: ", count)
+            i += 1
+        count += 1
+    return count
 
 if __name__ == '__main__':
     n = int(input())
